342903269ef249553265403d5ad644d4d9d69976
dai00086 <dai00086@umn.edu>
2022-05-30 16:08:02 -0500

    Update getWeather.py

--- getWeather.py
+++ getWeather.py
@@ -1,14 +1,17 @@
 import requests,json
 
-
 def getWeather():
   weatherURL = "https://api.openweathermap.org/data/2.5/weather?"
   APIKey = "355c78809c3b8837bda20915474303d0"
-  City = "Minneapolis"
-  URL = weatherURL + "q=" + City + "&APPID=" + APIKey
+  lattitude = "44.98891411861488"
+  longitidue = "-93.22727597285993"
+  URL = weatherURL + "lat=" + lattitude + "&lon=" + longitidue  + "&APPID=" + APIKey + "&units=imperial" 
   responseNumber = requests.get(URL)
   data = responseNumber.json()
-  main = data["main"]
-  print(main)
+  wind = data["wind"]
+  windSpeed = wind["speed"]
+  windStock = round(windSpeed)
+  return windStock
+  
     
 getWeather()
